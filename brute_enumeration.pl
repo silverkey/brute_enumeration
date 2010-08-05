193f8c9c24dcbf4746ea006af1f2608f7c6b03b9
silverkey <noncoding@gmail.com>
2010-08-05 17:39:28 +0200

    corrected indentation in brute_enumeration.pl

--- brute_enumeration.pl
+++ brute_enumeration.pl
@@ -78,43 +78,43 @@ Note    :
 =cut
 
 sub shuffle_fasta {
-	
+  
   my $x;
-	
+  
   for($x=1;$x<=$NUM_SH;$x++) {
     
     my $seqio = Bio::SeqIO->new(-file => $FASTA,
-		-format => 'fasta');
-		
+                                -format => 'fasta');
+    
     my $file_shu = "$FASTA\_shuffled\_$x\.fa";
     $file_shu =~ s/\.fa//;
-		
+    
     my $seqio_sh = Bio::SeqIO->new(-file => ">$file_shu",
-		-format => 'fasta');
-		
+                                   -format => 'fasta');
+    
     while(my $seq = $seqio->next_seq) {
-			
+      
       my @seq = split(//,$seq->seq);
-			
+      
       my $char = scalar(@seq)-1;
       my @order = (0..$char);
-			
+      
       my @new_order1 = _shuffle(\@order);
       my @new_order2 = _shuffle(\@new_order1);
       my @new_order = _shuffle(\@new_order2);
-			
+      
       my $string;
       foreach my $inx(@new_order) {
         $string .= $seq[$inx];
       }
-			
+      
       $string =~ s/N//g;
-			
+      
       my $seq_sh = Bio::Seq->new(-id => $seq->id,
-			-seq => $string);
-			
+                                 -seq => $string);
+      
       $seqio_sh->write_seq($seq_sh);
-			
+      
       $NUMSEQ ++ if $x == 1;
     }
   }
@@ -138,13 +138,13 @@ Note    : called by shuffle()
 =cut
 
 sub _shuffle {
-	return @_ if !@_ || ref $_ [0] eq 'ARRAY' && !@{$_ [0]};
-	my $array = @_ == 1 && ref $_ [0] eq 'ARRAY' ? shift : [@_];
-	for (my $i = @$array; -- $i;) {
-		my $r = int rand ($i + 1);
-		($array -> [$i], $array -> [$r]) = ($array -> [$r], $array -> [$i]);
-	}
-	wantarray ? @$array : $array;
+  return @_ if !@_ || ref $_ [0] eq 'ARRAY' && !@{$_ [0]};
+  my $array = @_ == 1 && ref $_ [0] eq 'ARRAY' ? shift : [@_];
+  for (my $i = @$array; -- $i;) {
+    my $r = int rand ($i + 1);
+    ($array -> [$i], $array -> [$r]) = ($array -> [$r], $array -> [$i]);
+  }
+  wantarray ? @$array : $array;
 }
 
 =head2 count_real
@@ -164,32 +164,32 @@ Note    :
 =cut
 
 sub count_real {
-	
+  
   open(OCC,">$file_occ");
   print OCC "ref_id\tword\tposition\n";
-	
+  
   my $seqio = Bio::SeqIO->new(-file => $FASTA,
-	-format => 'fasta');
-	
+                              -format => 'fasta');
+  
   while(my $seq = $seqio->next_seq) {
-		
+    
     my $string = $seq->seq;
     my $id = $seq->id;
-		
+    
     my $start = 0;
     my $last = length($string) - $WL;
-		
+    
     while($start <= $last) {
-			
+      
       my $substring = substr($string,$start,$WL);
-			
+      
       $start ++;
-			
+      
       next if $substring =~ /N/;
-			
+      
       $COUNTS->{$substring}->{'R'} ++;
       $GENES->{$substring}->{$id} = 1;
-			
+      
       print OCC "$id\t$substring\t".($start-1)."\n";
     }
   }
@@ -212,34 +212,34 @@ Note    :
 =cut
 
 sub count_shuffled {
-	
+  
   my $x;
-	
+  
   for($x=1;$x<=$NUM_SH;$x++) {
-		
+    
     my $file = "$FASTA\_shuffled_$x\.fa";
     $file =~ s/\.fa//;
-		
+    
     my $seqio = Bio::SeqIO->new(-file => $file,
-		-format => 'fasta');
-		
+                                -format => 'fasta');
+    
     while(my $seq = $seqio->next_seq) {
-			
+      
       my $string = $seq->seq;
       my $id = $seq->id;
-			
+      
       my $start = 0;
       my $last = length($string) - $WL;
-			
+      
       while($start <= $last) {
-				
+        
         my $substring = substr($string,$start,$WL);
-				
+        
         $start ++;
-				
+        
         next if $substring =~ /N/;
         next unless exists $COUNTS->{$substring};
-				
+        
         $COUNTS->{$substring}->{$x} ++;
         $SH_GENES->{$substring}->{$x}->{$id} = 1;
       }
@@ -264,32 +264,32 @@ Note    :
 =cut
 
 sub analize_res {
-	
+  
   open(COU,">$file_cou");
   print COU "\tnum_occ\tnum_seq\tz_score_occ\tp.value_occ\tadj.p.value_occ\tz_score_num_seq\tp.value_num_seq\tadj.p.value_num_seq\n";
-	
+  
   foreach my $word(keys %$COUNTS) {
-		
+    
     my $stat_c = Statistics::Descriptive::Full->new();
     my $stat_g = Statistics::Descriptive::Full->new();
-		
+    
     my $r = $COUNTS->{$word}->{'R'};
     my $num_seq = scalar(keys %{$GENES->{$word}});
-		
+    
     next if $num_seq < $MIN_SEQ;
-		
+    
     my $x;
     my @s_c;
     my @s_g;
     my $z_score_c;
     my $z_score_g;
-		
+    
     for($x=1;$x<=$NUM_SH;$x++) {
       if(exists $COUNTS->{$word}->{$x}) {
-				
+        
         my $count_c = $COUNTS->{$word}->{$x};
         push(@s_c,$count_c);
-				
+        
         my $count_g = scalar(keys %{$SH_GENES->{$word}->{$x}});
         push(@s_g,$count_g);
       }
@@ -300,13 +300,13 @@ sub analize_res {
     my $sd_c = $stat_c->standard_deviation;
     my $diff_c = $r - $mean_c;
     $z_score_c = $diff_c / ($sd_c || 1);
-		
+    
     $stat_g->add_data(@s_g);
     my $mean_g = $stat_g->mean;
     my $sd_g = $stat_g->standard_deviation;
     my $diff_g = $num_seq - $mean_g;
     $z_score_g = $diff_g / ($sd_g || 1);
-		
+    
     print COU "$word\t$r\t$num_seq\t";
     print COU "$z_score_c\tNA\tNA\t"; #if $z_score_c;
     print COU "$z_score_g\tNA\tNA\n"; #if $z_score_g;
@@ -315,32 +315,32 @@ sub analize_res {
 }
 
 sub calculate_p_value {
-	
+  
   my $script = 'options(echo=FALSE)'."\n";
-	
+  
   $script .=
-	'
-	f<-read.table(file="'.$file_cou.'")
-	
-	for(i in 1:nrow(f)) {
-		
-		f$p.value_occ[i] <- 1-pnorm(abs(f$z_score_occ[i]))
-		f$adj.p.value_occ[i] <- nrow(f)*(1-pnorm(abs(f$z_score_occ[i])))
-		
-		f$p.value_num_seq[i] <- 1-pnorm(abs(f$z_score_num_seq[i]))
-		f$adj.p.value_num_seq[i] <- nrow(f)*(1-pnorm(abs(f$z_score_num_seq[i])))
-	}
-	
-	write.csv(f,file="stat_'.$file_cou.'.csv")
-	';
+  '
+  f<-read.table(file="'.$file_cou.'")
+  
+  for(i in 1:nrow(f)) {
+    
+    f$p.value_occ[i] <- 1-pnorm(abs(f$z_score_occ[i]))
+    f$adj.p.value_occ[i] <- nrow(f)*(1-pnorm(abs(f$z_score_occ[i])))
+    
+    f$p.value_num_seq[i] <- 1-pnorm(abs(f$z_score_num_seq[i]))
+    f$adj.p.value_num_seq[i] <- nrow(f)*(1-pnorm(abs(f$z_score_num_seq[i])))
+  }
+  
+  write.csv(f,file="stat_'.$file_cou.'.csv")
+  ';
   
   my $filename = "SCRIPT_$ARGV[0]\_$ARGV[1]\_$ARGV[2]\_$ARGV[3]";
-	
+  
   open(SCRIPT,">$filename");
   print SCRIPT $script;
   
   system("R CMD BATCH --no-save $filename");
-	#  system("rm $filename\*");
+  #  system("rm $filename\*");
 }
 
 sub write_log {
